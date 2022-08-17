71a6ac63dcae28191c8056fb684dcccfabe89ea4
Tiago Villa <tiago_villa@hotmail.com>
2022-08-17 18:26:26 -0300

    first commit

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex fle-col sm:flex-row items-center gap-4 text-white py-6"></nav>
+
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
